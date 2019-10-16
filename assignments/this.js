45b03d9a61fb0b05a6d6c0fc56918cc07ecd722a
cstewart94 <shassey.mayhem@gmail.com>
2019-10-16 16:10:37 -0500

    started this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When nothing else applies, defaults to the window object.
+* 2. Left of the dot. Only objects with methods.
+* 3. Creates a new object
+* 4. Can state what the 'this' keyword is in a function
 *
 * write out a code example of each explanation above
 */
@@ -14,7 +14,16 @@
 // code example for Window Binding
 
 // Principle 2
-
+const exampleCat={
+    fur: 'short hair',
+    color: 'tabby',
+    name: 'Frankie',
+    attitude: 'inquisitive',
+    aboutCat:function(){
+      console.log (`${this.name} is a ${this.fur} ${this.color} and is very ${this.attitude}.`);
+    }
+  }
+  exampleCat.aboutCat();
 // code example for Implicit Binding
 
 // Principle 3
